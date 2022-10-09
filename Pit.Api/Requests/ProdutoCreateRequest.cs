83d43db9d2033ea43b43d7aaa12bcedff4ef6a4f
Daniel <xdanielrodrigues@hotmail.com>
2022-10-09 13:04:16 -0300

    request criar produto, update método de atualização

---
+++ Pit.Api/Requests/ProdutoCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Pit.Api.Requests;
+
+public class ProdutoCreateRequest
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string CodigoBarras { get; set; }
+    public decimal Valor { get; set; }
+
+}
--- Pit.Api/Services/Interfaces/IProdutoService.cs
+++ Pit.Api/Services/Interfaces/IProdutoService.cs
@@ -5,9 +5,9 @@ namespace Pit.Api.Services.Interfaces;
 
 public interface IProdutoService
 {
-    Produto AdicionarProduto(Produto produto);
+    Produto AdicionarProduto(ProdutoCreateRequest produto);
     bool DeletarProduto(int? id);
     Task<List<Produto>> ObterProdutosAtivos();
     Task<List<Produto>> BuscarProdutoAsync(string filtro);
-    Task<Produto> AtualizarProdutoAsync(ProdutoDto produtoDto);
+    Task<Produto> AtualizarProdutoAsync(ProdutoDto produtoDto, int id);
 }
